8e1f315c21d5bcd60fa8a34495c4ef2ca64271b1
Bas <baaspaap@live.nl>
2014-07-21 18:26:24 +0200

    Reveal getriggerd door handpositie.

---
+++ Sphere/HandDistanceToBooleanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Bas.Sphere
+{
+    class HandDistanceToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double)
+            {
+                return (double)value == 1.0;
+            }
+
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
