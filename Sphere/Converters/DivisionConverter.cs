f11de09d4a7518094b66dce4764b1005ef450cd3
Bas <baspaap@live.nl>
2014-07-30 07:15:28 +0200

    Test image toegevoegd.

---
+++ Sphere/Converters/DivisionConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Bas.Sphere.Converters
+{
+    public class DivisionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            double doubleParameter;
+
+            if (value is double && double.TryParse((string)parameter, NumberStyles.Float, CultureInfo.InvariantCulture, out doubleParameter))
+            {
+                return (double)value / doubleParameter;
+            }
+
+            return 1.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Sphere/TestImage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bas.Sphere
+{
+    /// <summary>
+    /// Interaction logic for TestImage.xaml
+    /// </summary>
+    public partial class TestImage : UserControl, INotifyPropertyChanged
+    {
+        public TestImage()
+        {
+            InitializeComponent();
+
+            this.SizeChanged += TestImage_SizeChanged;
+        }
+
+        void TestImage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FrontDotMargin = new Thickness(0.0, this.ActualHeight / 2.0, 0.0, 0.0);
+
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("FrontDotMargin"));
+            }
+        }
+
+        public Thickness FrontDotMargin { get; set; }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
