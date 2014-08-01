7eb0286ef0cb2ad74367541e3e7823e3fb4f8f5d
Bas <baaspaap@live.nl>
2014-08-01 09:43:44 +0200

    Commentaar

--- Sphere/HandTracking/Hands.cs
+++ Sphere/HandTracking/Hands.cs
@@ -7,16 +7,19 @@ using System.Threading.Tasks;
 namespace Bas.Sphere.HandTracking
 {
     public class Hands
-    {
+    {        
         public bool IsEnabled
         {
-            get;
-            set;
+            get; // Geef de status van de leap terug
+            set; // Enable of disable de leap;
         }
 
-        public event EventHandler<HandProximityChangedEventArgs> HandProximityChanged;
-        public event EventHandler<VisionSummonedEventArgs> VisionSummoned;
+        public event EventHandler<HandProximityChangedEventArgs> HandProximityChanged; // Wordt gefired als er een wijziging in de handproximity is
+        public event EventHandler<VisionSummonedEventArgs> VisionSummoned; // Wordt gefired als iemand een bepaald vision-gebaar maakt.
 
+        /// <summary>
+        /// Stores the current hand position as the beginning of the "active" zone (where proximity is 1).
+        /// </summary>
         public void Calibrate()
         {
             if (IsEnabled)
