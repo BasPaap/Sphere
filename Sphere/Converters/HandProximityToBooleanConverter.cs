9ea3117c4b9e282af48e04ea37368f8e738fa163
Bas <baaspaap@live.nl>
2014-09-02 00:34:42 +0200

    De starburst blijft nu zichtbaar als hij eenmaal is verschenen, tenzij de handen echt helemaal verdwijnen.

--- Sphere/Converters/HandProximityToBooleanConverter.cs
+++ Sphere/Converters/HandProximityToBooleanConverter.cs
@@ -9,11 +9,26 @@ namespace Bas.Sphere.Converters
 {
     class HandProximityToBooleanConverter : IValueConverter
     {
+        private bool isRevealed = false;
+
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             if (value is double)
             {
-                return (double)value == 1.0;
+                var proximity = (double)value;
+
+                if (isRevealed)
+                {
+                    // If the starburst has already been revealed, just keep saying it's revealed unless hand proximity is absolutely zero.
+                    isRevealed = proximity != 0.0;                    
+                }
+                else
+                {
+                    // If the starburst is not yet revealed, it will be when proximity equals 1.0.
+                    isRevealed = proximity == 1.0;
+                }
+
+                return isRevealed;
             }
 
             return false;
